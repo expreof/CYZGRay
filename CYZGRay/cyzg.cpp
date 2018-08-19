e268119310ea51181ff1ef9e9597a26365c0d822
expreof <lsx0626@outlook.com>
2018-08-19 16:41:40 +0800

    删除测试代码

--- CYZGRay/cyzg.cpp
+++ CYZGRay/cyzg.cpp
@@ -2,16 +2,7 @@
 #include <cassert>
 #include <cmath>
 
-void print(size_t size)
-{
-#ifndef NDEBUG
-	std::cerr << __func__ << ": array size is " << size << std::endl;
-#endif
-}
-
 int main()
 {
-	auto a = INFINITY;
-	std::cout << a;
 	return 0;
 }
